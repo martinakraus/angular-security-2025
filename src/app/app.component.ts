f619048546e46955bd2135116f7dfd1bffee45cc
Martina Kraus <kraus.martina.m@gmail.com>
2025-03-17 22:52:57 +0100

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, inject, ViewChild } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-root",
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: "./app.component.html",
-  imports: [RouterOutlet],
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {}
+export class AppComponent {
+  sanitizer = inject(DomSanitizer);
+  value = `Hello!<img src="none" onerror="alert('This data has become code!')">`;
+  renderedValue = "";
+  unsafeRenderedValue!: SafeHtml;
+
+  @ViewChild("divRender3") div!: ElementRef;
+
+  renderData() {
+    // Div 1
+    this.renderedValue = this.value;
+
+    // Div 2
+    this.unsafeRenderedValue = this.sanitizer.bypassSecurityTrustHtml(
+      this.value
+    );
+
+    // Div 3
+    this.div.nativeElement.innerHTML = this.value;
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideHttpClient, withXsrfConfiguration } from "@angular/common/http";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
-import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())],
+  providers: [],
 };
