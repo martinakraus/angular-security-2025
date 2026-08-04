2a05d9a52e32faf88f77bd5a62c5fab8195cdece
Martina Kraus <kraus.martina.m@gmail.com>
2026-08-04 11:33:41 +0200

    bump to version 22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
+  selector: 'app-root',
+  templateUrl: './app.component.html',
   imports: [RouterOutlet],
-  styleUrl: "./app.component.scss",
+  changeDetection: ChangeDetectionStrategy.Eager,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {}
