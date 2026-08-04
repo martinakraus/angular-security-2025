f6c91163039315f6f08ee1ad51ea721050044fb0
Martina Kraus <kraus.martina.m@gmail.com>
2026-08-04 11:47:44 +0200

    bump to version 22

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig } from "@angular/core";
-import { provideHttpClient, withXsrfConfiguration } from "@angular/common/http";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
-import { routes } from "./app.routes";
+import { ApplicationConfig } from '@angular/core';
+import {
+  provideHttpClient,
+  withXsrfConfiguration,
+  withXhr,
+} from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withXsrfConfiguration({})),
+    provideHttpClient(withXhr(), withXsrfConfiguration({})),
     provideRouter(routes, withComponentInputBinding()),
   ],
 };
